8c03b1b0903359d2ff83a18adaac13c7e62a3d04
MrWako <mark.a.wakefield@gmail.com>
2017-04-30 21:25:44 +0100

    Mini victory, display a list.

--- main.js
+++ main.js
@@ -2,14 +2,59 @@ import Expo from 'expo';
 import React from 'react';
 import {
   StyleSheet,
-  StatusBar,
-  Text,
-  View,
+  StatusBar
 } from 'react-native';
 
-import { Examples } from '@shoutem/ui';
+import { Divider, ListView, Text, View, Image, Tile, Title, Subtitle, Screen} from '@shoutem/ui';
 
 console.disableYellowBox = true;
+
+class ListTest extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.renderRow = this.renderRow.bind(this);
+    this.state = {
+      restaurants: [{
+        "name": "Gaspar Brasserie",
+        "address": "185 Sutter St, San Francisco, CA 94109",
+        "image": { "url": "https://shoutem.github.io/restaurants/restaurant-1.jpg" },
+      }, {
+        "name": "Chalk Point Kitchen 2",
+        "address": "527 Broome St, New York, NY 10013",
+        "image": { "url": "https://shoutem.github.io/restaurants/restaurant-2.jpg" },
+      }],
+    }
+  }
+
+  renderRow(restaurant) {
+    return (
+      <View>
+        <Image
+          styleName="large-banner"
+          source={{ uri: restaurant.image.url }}
+        >
+          <Tile>
+            <Title styleName="md-gutter-bottom">{restaurant.name}</Title>
+            <Subtitle styleName="sm-gutter-horizontal">{restaurant.address}</Subtitle>
+          </Tile>
+        </Image>
+        <Divider styleName="line" />
+      </View>
+    );
+  }
+
+  render() {
+    return (
+      <Screen>
+        <ListView
+          data={this.state.restaurants}
+          renderRow={this.renderRow}
+        />
+      </Screen>
+    );
+  }
+}
 
 class App extends React.Component {
   state = {
@@ -41,8 +86,7 @@ class App extends React.Component {
 
     return (
       <View style={{flex: 1}}>
-        <Examples />
-        <StatusBar barStyle="default" hidden={false} />
+        <ListTest/>
       </View>
     );
   }
