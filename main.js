39074c53f8ff869480a0d2c84267e792367cc2e1
MrWako <mark.a.wakefield@gmail.com>
2017-05-01 19:35:52 +0100

    Try and get redux plumbed in.

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
 import Expo from 'expo';
+import React from 'react';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { reducer } from './containers/redux.js';
 import { App } from './containers/App.js';
 
 console.disableYellowBox = true;
-Expo.registerRootComponent(App);
+const store = createStore(reducer);
+
+class Root extends React.Component {
+    render() {
+      return (
+        <Provider store={store}>
+          <App />
+        </Provider>);
+      }
+}
+
+Expo.registerRootComponent(Root);
