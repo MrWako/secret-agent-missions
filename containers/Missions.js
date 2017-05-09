6440003e4ec9ce44bf430fde0db3d6ebbacddfdb
MrWako <mark.a.wakefield@gmail.com>
2017-05-09 21:32:47 +0100

    Baby steps.

--- containers/Missions.js
+++ containers/Missions.js
@@ -1,3 +1,4 @@
+import { fromJS, toJS } from 'immutable';
 import React from 'react';
 import { connect } from 'react-redux'
 
@@ -6,7 +7,7 @@ import { MissionsList } from '../components/MissionsList.js';
 // this is where we should convert state to props
 function mapStateToProps(state) {
   return {
-    restaurants: [{
+    restaurants: fromJS([{
       "name": "Gaspar Brasserie",
       "address": "185 Sutter St, San Francisco, CA 94109",
       "image": { "url": "https://shoutem.github.io/restaurants/restaurant-1.jpg" },
@@ -14,14 +15,14 @@ function mapStateToProps(state) {
       "name": "Chalk Point Kitchen",
       "address": "527 Broome St, New York, NY 10013",
       "image": { "url": "https://shoutem.github.io/restaurants/restaurant-2.jpg" },
-    }]
+    }])
   };
 }
 
-function toJS(WrappedComponent) {
-  return function(wrappedComponentProps) {
-    return <WrappedComponent restaurants={wrappedComponentProps.restaurants} />
+function wrap(Component) {
+  return function(props) {
+    return <Component restaurants={props.restaurants.toJS()} />
   }
 }
 
-export const Missions = connect(mapStateToProps)(toJS(MissionsList));
+export const Missions = connect(mapStateToProps)(wrap(MissionsList));
