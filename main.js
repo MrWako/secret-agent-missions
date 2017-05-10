de8f3a16fd6680dca321b74f1773f84e12afea9f
MrWako <mark.a.wakefield@gmail.com>
2017-05-10 21:40:13 +0100

    experiment with react-navigation.

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 import Expo from 'expo';
 import Immutable from 'immutable';
 import React from 'react';
+import { StackNavigator } from 'react-navigation';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import { combineReducers } from 'redux-immutable';
 
+import { Button, Text, View } from 'react-native';
+
+/*
 import { missionsReducer } from './store/missionsReducer.js';
 import { App } from './containers/App.js';
 
@@ -24,3 +28,43 @@ class Root extends React.Component {
 }
 
 Expo.registerRootComponent(Root);
+*/
+
+class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Welcome',
+  };
+  render() {
+    const { navigate } = this.props.navigation;
+    return (
+      <View>
+        <Text>Hello, Chat App!</Text>
+        <Button
+          onPress={() => navigate('Chat')}
+          title="Chat with Lucy"
+        />
+      </View>
+    );
+  }
+}
+
+
+class ChatScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Chat with Lucy',
+  };
+  render() {
+    return (
+      <View>
+        <Text>Chat with Lucy</Text>
+      </View>
+    );
+  }
+}
+
+const SimpleApp = StackNavigator({
+  Home: { screen: HomeScreen },
+  Chat: { screen: ChatScreen },
+});
+
+Expo.registerRootComponent(SimpleApp);
