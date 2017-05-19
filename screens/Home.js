b32202d24beb91b0502df44c0b9a3fe482b12b0f
MrWako <mark.a.wakefield@gmail.com>
2017-05-19 19:19:08 +0100

    Make a start with gesture password.

--- screens/Home.js
+++ screens/Home.js
@@ -2,15 +2,23 @@ import Expo from 'expo';
 import React from 'react';
 import { Text, View } from 'react-native';
 
-import { Missions } from '../containers/Missions.js';
+import { Keypad } from '../components/Keypad.js';
 
 export class Home extends React.PureComponent {
 
   render() {
     return (
       <View style={{flex: 1}}>
+        <Keypad/>
+      </View>
+    );
+
+    /*
+    return (
+      <View style={{flex: 1}}>
         <Missions navigation={this.props.navigation}/>
       </View>
     );
+    */
   }
 }
