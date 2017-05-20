f902b9395d30462a8863d5b6888f1db09161c37d
MrWako <mark.a.wakefield@gmail.com>
2017-05-20 17:49:07 +0100

    Experiment with barcode scanner component.

--- screens/Home.js
+++ screens/Home.js
@@ -3,26 +3,26 @@ import React from 'react';
 import { Text, View } from 'react-native';
 
 import { Keypad } from '../components/Keypad.js';
+import { BarcodeScanner } from '../components/BarcodeScanner.js';
 import { Missions } from '../containers/Missions.js';
 
 export class Home extends React.PureComponent {
 
   render() {
 
-    /*
     return (
       <View style={{flex: 1}}>
-        <Keypad/>
+        <BarcodeScanner/>
       </View>
     );
-    */
 
 
+    /*
     return (
       <View style={{flex: 1}}>
         <Missions navigation={this.props.navigation}/>
       </View>
     );
-
+    */
   }
 }
